cd1f3245784a5e2c1f04a3fa82b18c14f8c7d5ef
DenysMelnychuk1 <meldenys1907@gmail.com>
2023-11-29 11:55:57 +0200

    1
    1

---
+++ main.py
@@ -0,0 +1,60 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate()
+
+    def validate(self):
+        if not (isinstance(self.value, str) and self.value.isdigit() and len(self.value) == 10):
+            raise ValueError("Phone number must be a 10-digit string")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        new_phone = Phone(phone)
+        self.phones.append(new_phone)
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        if not any(p.value == old_phone for p in self.phones):
+            raise ValueError(f"Phone {old_phone} not found in the record")
+        
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
+        return None
+
+    def __str__(self):
+        phone_str = '; '.join(str(p) for p in self.phones)
+        return f"Contact name: {self.name}, phones: {phone_str}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
